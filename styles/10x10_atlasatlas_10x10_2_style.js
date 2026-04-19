f149105d86219c887363218064e337d7502643eb
ittelozzaf <david.dopereiro@gmail.com>
2026-04-19 23:26:43 +0200

    Update 10x10_atlasatlas_10x10_2_style.js

--- styles/10x10_atlasatlas_10x10_2_style.js
+++ styles/10x10_atlasatlas_10x10_2_style.js
@@ -7,8 +7,7 @@ var style_10x10_atlasatlas_10x10_2 = function(feature, resolution){
         variables: {}
     };
     
-    var labelText = ""; 
-    var value = feature.get("");
+    var labelText = ""; var value = feature.get("");
     var labelFont = "10px, sans-serif";
     var labelFill = "#000000";
     var bufferColor = "";
